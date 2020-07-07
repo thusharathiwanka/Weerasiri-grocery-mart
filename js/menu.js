ae28f8c7c9ef6214e61d2ac5a6ccd36a37ea38d7
thusharathiwanka <thusharathiwank123@gmail.com>
2020-07-08 03:47:54 +0530

    fixed hamburger menu issue

--- js/menu.js
+++ js/menu.js
@@ -1,18 +1,18 @@
 const nav = document.querySelector(".nav-links");
-const menuOpen = document.querySelector(".menu");
-const menuClose = document.querySelector(".close");
+const menuOpen = document.querySelector("#menu");
+const menuClose = document.querySelector("#close");
 const links = document.querySelectorAll(".nav-links li a");
 
 menuOpen.addEventListener("click", () => {
   nav.classList.toggle("nav-open");
-  menuClose.style.display = "block";
-  menuOpen.style.display = "none";
+  setTimeout(() => {
+    menuClose.style.display = "block";
+  }, 100);
 });
 
 menuClose.addEventListener("click", () => {
   nav.classList.toggle("nav-open");
   menuClose.style.display = "none";
-  menuOpen.style.display = "block";
 });
 
 links.forEach((link) => {
