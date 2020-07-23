8b3cb14ae76e0dde08e121dbbfe3897ac4ea2d5d
thusharathiwanka <thusharathiwank123@gmail.com>
2020-07-23 18:32:27 +0530

    delete functionality added

---
+++ includes/delete_profile_inc.php
@@ -0,0 +1,20 @@
+<?php
+   session_start();
+   include_once 'db_conn_inc.php';
+   
+   $customerID = $_SESSION['customer_id'];
+   $sql = "DELETE FROM customer WHERE customer_id='$customerID'";
+   $result = mysqli_query($conn, $sql);
+
+   if(!$result) {
+      header("Location: ../customer_profile.php?delete=unsuccess");
+      exit();
+   } else {
+      header("Location: ../index.html?delete=success");
+      exit();
+
+      session_unset();
+      session_destroy();
+   }
+   
+?>
--- includes/logout_inc.php
+++ includes/logout_inc.php
@@ -2,9 +2,12 @@
    if(isset($_POST['submit'])) {
       session_start();
       session_unset();
-      session_unset();
+      session_destroy();
 
-      header("Location: ../index.html");
+      header("Location: ../index.html?logout=success");
+      exit();
+   } else {
+      header("Location: ../customer_profile.php?logout=unsuccess");
       exit();
    }
 ?>
