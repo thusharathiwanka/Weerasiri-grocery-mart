4509d4a077017b3291c1d16c57a74105003489e3
thusharathiwanka <thusharathiwanka123@gmail.com>
2020-07-25 17:49:21 +0530

    added js comments

--- js/headsup.js
+++ js/headsup.js
@@ -1,6 +1,8 @@
+//Selecting elements from DOM
 const error = document.querySelector(".error");
 const success = document.querySelector(".success");
 
+//Checking if it is error or success
 if (error != null) {
   setTimeout(() => {
     error.style.display = "none";
--- js/menu.js
+++ js/menu.js
@@ -1,8 +1,10 @@
+//Selecting elements from DOM
 const nav = document.querySelector(".nav-links");
 const menuOpen = document.querySelector("#menu");
 const menuClose = document.querySelector("#close");
 const links = nav.querySelectorAll("li a");
 
+//Setting up menu click
 menuOpen.addEventListener("click", () => {
   nav.classList.toggle("nav-open");
   setTimeout(() => {
@@ -10,11 +12,13 @@ menuOpen.addEventListener("click", () => {
   }, 100);
 });
 
+//Setting up menu close click
 menuClose.addEventListener("click", () => {
   nav.classList.toggle("nav-open");
   menuClose.style.display = "none";
 });
 
+//Setting up navigation menu clicks
 links.forEach((link) => {
   link.addEventListener("click", () => {
     nav.classList.toggle("nav-open");
