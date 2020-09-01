62debd04d09695e160f111c6f96dac2a36dc2a26
thusharathiwanka <thusharathiwanka123@gmail.com>
2020-09-01 11:01:40 +0530

    navigation section changed

--- index.php
+++ index.php
@@ -33,6 +33,7 @@
                   <h2>Weerasiri <span>Grocery Mart</span></h2>
                </div>
                <ul class="nav-links">
+                  <li><a href="./index.php">Home</a></li>
                   <li><a href="#about">About</a></li>
                   <li><a href="#services">Services</a></li>
                   <li><a href="#contact">Contact</a></li>
--- sign_up.php
+++ sign_up.php
@@ -20,6 +20,7 @@
                </a>
             </div>
             <ul class="nav-links">
+               <li><a href="./index.php">Home</a></li>
                <li><a href="./#about">About</a></li>
                <li><a href="./#services">Services</a></li>
                <li><a href="./#contact">Contact</a></li>
