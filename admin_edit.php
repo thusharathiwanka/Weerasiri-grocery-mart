aa405838f8e57f05999f07b11627d80c1959cc61
thusharathiwanka <thusharathiwanka123@gmail.com>
2020-09-18 17:01:50 +0530

    admin edit functionality added

--- admin_edit.php
+++ admin_edit.php
@@ -68,10 +68,12 @@
                         echo "<div class='status-field'><p class='error edit-success'>This username is already taken</p></div>";
                      } else if($checkEdit == "mobile_invalid") {
                         echo "<div class='status-field'><p class='error edit-success'>Enter a valid mobile number</p></div>";
-                     } else if($checkEdit == "old_password_invalid") {
+                     } else if($checkEdit == "owner_password_invalid") {
                         echo "<div class='status-field'><p class='error edit-success'>Your current password is invalid</p></div>";
+                     } else if($checkEdit == "unsuccess") {
+                        echo "<div class='status-field'><p class='error edit-success'>Something went wrong</p></div>";
                      } else if($checkEdit == "success") {
-                        echo "<div class='status-field'><p class='success edit-success'>Your account has been updated successfully</p></div>";
+                        echo "<div class='status-field'><p class='success edit-success'>Details has been updated successfully</p></div>";
                      }
                   }
                   echo'<main>
--- includes/edit_admin_inc.php
+++ includes/edit_admin_inc.php
@@ -10,53 +10,51 @@
       $username = mysqli_real_escape_string($conn, $_POST['username']);
       $newPassword = mysqli_real_escape_string($conn, $_POST['new_password']);
       $mobile = mysqli_real_escape_string($conn, $_POST['mobile']);
+      $salary = mysqli_real_escape_string($conn, $_POST['salary']);
       $acc_no = mysqli_real_escape_string($conn, $_POST['acc_no']);
       $bank = mysqli_real_escape_string($conn, $_POST['bank']);
-      $currentPassword = mysqli_real_escape_string($conn, $_POST['current_password']);
+      $ownerPassword = mysqli_real_escape_string($conn, $_POST['current_password']);
 
       //Error handling
       //Checking for empty inputs
-      if(empty($name) || empty($email) || empty($username) || empty($newPassword) || empty($mobile) || empty($acc_no) || empty($bank) || empty($currentPassword)) {
-         header("Location: ../admin_edit_profile.php?edit=empty");
+      if(empty($name) || empty($email) || empty($username) || empty($newPassword) || empty($mobile) || empty($salary) || empty($acc_no) || empty($bank) || empty($ownerPassword)) {
+         header("Location: ../admin_edit.php?edit=empty");
          exit();
       } else {
          //Checking for valid name
          if(!preg_match("/^[a-zA-Z ]*$/", $name)) {
-            header("Location: ../admin_edit_profile.php?edit=name_invalid");
+            header("Location: ../admin_edit.php?edit=name_invalid");
             exit();
          } else {
             //Checking for valid email
             if(!filter_var($email, FILTER_VALIDATE_EMAIL)) {
-               header("Location: ../admin_edit_profile.php?edit=email_invalid");
+               header("Location: ../admin_edit.php?edit=email_invalid");
                exit();
             } else {
                //Checking for invalid mobile number
                if(!preg_match("/^[0-9]*$/", $mobile) || strlen($mobile) != 10) {
-                  header("Location: ../admin_edit_profile.php?edit=mobile_invalid");
+                  header("Location: ../admin_edit.php?edit=mobile_invalid");
                   exit();
                } else {
-                  $OldPassword =  $_SESSION['admin_password'];
-                  
-                  if($OldPassword != $currentPassword) {
-                     header("Location: ../admin_edit_profile.php?edit=old_password_invalid");
+                     $sql = "SELECT * FROM admin WHERE admin_password='$ownerPassword' AND admin_type='Owner'";
+                     $result = mysqli_query($conn, $sql);
+                     $checkResult = mysqli_num_rows($result);
+
+                  if($checkResult < 0) {
+                     header("Location: ../admin_edit.php?edit=owner_password_invalid");
                      exit();
                   } else {
                      //Updating user to table
-                     $adminID = $_SESSION['admin_id'];
-                     $sql = "UPDATE admin SET admin_name='$name', admin_email='$email', admin_username='$username', admin_password='$newPassword', admin_mobile='$mobile', admin_acc_no='$acc_no', admin_bank='$bank' WHERE admin_id='$adminID'";
+                     $sql = "UPDATE admin SET admin_name='$name', admin_email='$email', admin_username='$username', admin_password='$newPassword', admin_mobileno='$mobile', admin_salary='$salary', admin_bank_acc_no='$acc_no', admin_bank='$bank' WHERE admin_username='$username'";
                      $result = mysqli_query($conn, $sql);
 
-                     //Updating sessions
-                     $_SESSION['admin_name'] = $name;
-                     $_SESSION['admin_email'] = $email;
-                     $_SESSION['admin_username'] = $username;
-                     $_SESSION['admin_password'] = $newPassword;
-                     $_SESSION['admin_mobile'] = $mobile;
-                     $_SESSION['admin_acc_no'] = $acc_no;
-                     $_SESSION['admin_bank'] = $bank;
-
-                     header("Location: ../admin_edit_profile.php?edit=success");
-                     exit();
+                     if(!$result) {
+                        header("Location: ../admin_edit.php?edit=unsuccess");
+                        exit();
+                     } else {
+                        header("Location: ../admin_edit.php?edit=success");
+                        exit();
+                     }
                   }
                }
             }
