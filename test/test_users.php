071834e16ca22a45f7352c5aabaf4ae784193638
thusharathiwanka <thusharathiwanka123@gmail.com>
2020-10-11 19:16:22 +0530

    add: testing added

---
+++ test/test_users.php
@@ -0,0 +1,16 @@
+<?php
+   use PHPUnit\Framework\TestCase;
+
+   /**
+    * Testing for profile management function
+    * IT19076362
+    */
+   class UserTest extends TestCase {
+      /** 
+       * @test 
+      */
+      public function userDetailsValidation() {
+         $this->assertEquals(true);
+      }
+   } 
+?>
